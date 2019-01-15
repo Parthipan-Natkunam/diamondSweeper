66f897d300ca636c3d7895bf40e44ebb671e45be
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-01-15 16:19:46 +0530

    Adding click handlers and means to track score and found diamonds

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -6,7 +6,8 @@ class GameContainer extends React.Component{
     state = {
         tiles: [],
         score: 64,
-        totalTiles: 64
+        totalTiles: 64,
+        foundDiamonds: 0
     }
     componentDidMount(){
         if(this.state.tiles.length > 0) {return}
@@ -29,13 +30,28 @@ class GameContainer extends React.Component{
             this.setState({tiles});
         }
     }
+    changeOpenState(tilePosition){
+        let tileStates = this.state.tiles.slice(); //slicing to prevent state mutation
+        let diamondsFound = this.state.foundDiamonds;
+        let currentScore = this.state.score;
+        if(!tileStates[tilePosition].isOpen){
+            tileStates[tilePosition].isOpen = !tileStates[tilePosition].isOpen;
+        }
+        if(tileStates[tilePosition].hasDiamond){ diamondsFound++; }
+        else{ currentScore--; }
+        this.setState({
+            tiles: tileStates,
+            foundDiamonds: diamondsFound,
+            score: currentScore
+        });
+    }
     render(){
         return(
             <div className="game-container">
                 {
                     Array(this.state.totalTiles).fill('0').map((item,index) => {
                         return(
-                            <Tile key={index} position={index} tileState={this.state.tiles[index]}/>
+                            <Tile key={index} position={index} tileState={this.state.tiles[index]} clickHandler={this.changeOpenState.bind(this)}/>
                         ); 
                     })
                 }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 class Tile extends React.Component{
+    callClickHandler(){
+        this.props.clickHandler(this.props.position);
+    }
     render(){
-        let {position,tileState} = this.props;
+        let {tileState} = this.props;
         let cssClasses = ["game-tile"];
         if(tileState){
             if(tileState.isOpen && tileState.hasDiamond) {cssClasses.push(" diamond")}
             else if(!tileState.isOpen) { cssClasses.push(" unknown")}
         }
         return(
-            <div className= {cssClasses.join('')} position={position}></div>
+            <div className= {cssClasses.join('')} onClick={this.callClickHandler.bind(this)}></div>
         );
     }
 }
